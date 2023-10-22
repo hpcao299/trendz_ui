98fbdf96538dd110133831d2b5c20118aaf75f4f
HPhucJ2007 <yourgitemail@example.com>
2023-10-22 16:04:58 +0700

    guest profile page ui

--- src/app/[username]/page.tsx
+++ src/app/[username]/page.tsx
@@ -6,7 +6,7 @@ import { useLayoutStore } from '@/stores';
 import { NextPage } from 'next';
 import Image from 'next/image';
 import Link from 'next/link';
-import { IoChevronDown, IoSettingsOutline } from 'react-icons/io5';
+import { IoChatbubble, IoChevronDown, IoHeart, IoSettingsOutline } from 'react-icons/io5';
 
 interface ProfilePageProps {
     params: { username: string };
@@ -74,7 +74,7 @@ const ProfilePage: NextPage<ProfilePageProps> = ({ params }) => {
                     </div>
                 </header>
                 <div className="border-t border-solid border-separator">
-                    <div className="max-w-[350px] mx-auto my-[60px] flex flex-col items-center justify-center text-center">
+                    {/* <div className="max-w-[350px] mx-auto my-[60px] flex flex-col items-center justify-center text-center">
                         <div className="bg-photo h-[62px] w-[62px]"></div>
                         <div className="mt-5 mb-4 text-3xl font-extrabold">Share Photos</div>
                         <div className="mb-5 text-sm">
@@ -86,6 +86,48 @@ const ProfilePage: NextPage<ProfilePageProps> = ({ params }) => {
                         >
                             Share your first photo
                         </div>
+                    </div> */}
+                    <div className="flex flex-wrap gap-1 mt-1 mb-6">
+                        {Array.from({ length: 13 }).map((a, i) => (
+                            <div className="relative flex-[calc(33.33%-8px)] max-w-[calc(33.33%-8px)] w-[calc(33.33%-8px)] group">
+                                <Link href="/" key={i} className="block">
+                                    <div className="w-full overflow-hidden pb-[100%]">
+                                        <Image
+                                            src="/example_post1.jpeg"
+                                            alt="Photo by Thanh Thuy"
+                                            width={309}
+                                            height={309}
+                                            className="absolute top-0 left-0 object-cover w-full h-full"
+                                        />
+                                    </div>
+                                    <div className="absolute top-0 left-0 right-0 bottom-0 z-10 bg-[rgb(0,0,0,0.3)] opacity-0 group-hover:opacity-100 flex items-center justify-center">
+                                        <div className="flex items-center text-base font-bold text-white">
+                                            <div className="flex items-center mr-7">
+                                                <IoHeart size={20} />
+                                                <span className="ml-1.5">78</span>
+                                            </div>
+                                            <div className="flex items-center">
+                                                <IoChatbubble size={20} />
+                                                <span className="ml-1.5">78</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="absolute text-white top-2 right-2">
+                                        <svg
+                                            aria-label="Carousel"
+                                            fill="currentColor"
+                                            height="22"
+                                            role="img"
+                                            viewBox="0 0 48 48"
+                                            width="22"
+                                        >
+                                            <title>Carousel</title>
+                                            <path d="M34.8 29.7V11c0-2.9-2.3-5.2-5.2-5.2H11c-2.9 0-5.2 2.3-5.2 5.2v18.7c0 2.9 2.3 5.2 5.2 5.2h18.7c2.8-.1 5.1-2.4 5.1-5.2zM39.2 15v16.1c0 4.5-3.7 8.2-8.2 8.2H14.9c-.6 0-.9.7-.5 1.1 1 1.1 2.4 1.8 4.1 1.8h13.4c5.7 0 10.3-4.6 10.3-10.3V18.5c0-1.6-.7-3.1-1.8-4.1-.5-.4-1.2 0-1.2.6z"></path>
+                                        </svg>
+                                    </div>
+                                </Link>
+                            </div>
+                        ))}
                     </div>
                 </div>
             </div>
